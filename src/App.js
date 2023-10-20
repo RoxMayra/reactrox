bba3be10c26d599115e78d0cc2d73a70ddb53133
BerlyGus <berly.gustav@gmail.com>
2023-10-20 09:40:56 -0500

    v6 Actualizacion firebase

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
         <Vari/>
         <i class="large material-icons">insert_chart</i>
 
-        <p>1. Juan Manuel 23 Masculino ----- x  A</p> 
-        <p>2. Rosa Maria 22 Femenino  -----x  A</p> 
-        <p>3. Roxana Puma 25 Femenino ------ x  A</p> 
-        <p>4. Diego Lopez 28 Masculino ------ x  A</p> 
+        <p>1. Juan Manuel 23 Masculino ----- x --- A</p> 
+        <p>2. Rosa Maria 22 Femenino  -----x --- A</p> 
+        <p>3. Roxana Puma 25 Femenino ------ x --- A</p> 
+        <p>4. Diego Lopez 28 Masculino ------ x --- A</p> 
         
  
     </div>
